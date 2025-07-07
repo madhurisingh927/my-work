ed29e228e2fb325a32efc901e3b1aa673cebd55f
madhurisingh927 <singmadhurri@gmail.com>
2025-07-07 17:44:49 +0530

    movefile3

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
 mera bharat mahan hai
+resource "azurerm_resource_group" "myrgg" {
+  name = "mystudybook"
+  location = "westus"
+}
+resource "azurerm_resource_group" "xyz" {
+  name = "myutilise6789"
+  location = "westus"
+}
